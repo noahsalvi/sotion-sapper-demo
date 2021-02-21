34e40b19d956df63aeee004f6f45430d3b021c88
Noah Salvi <noah.salvi@ti8m.ch>
2021-02-21 22:40:13 +0100

    initial commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,24 @@
+<script context="module" lang="ts">
+  import { sotion } from "sotion";
+
+  export async function preload() {
+    sotion.setScope("765b1258c01c4c8caae4120790e4805a").setFetch(this.fetch);
+  }
+</script>
+
+<script lang="ts">
+  export let segment: string;
+  segment;
+</script>
+
+<div>
+  <slot />
+</div>
+
+<style>
+  div {
+    padding: 0 40px;
+    max-width: 900px;
+    margin: 0 auto;
+  }
+</style>
